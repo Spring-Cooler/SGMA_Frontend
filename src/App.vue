6c969863606e14584796b3cf79a62490137a5ac9
1etterh <1etterh.dev@gmail.com>
2024-09-25 11:44:44 +0900

    Refactor: 폴더 구조 초기화

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
-<template class="root-container">
-  <Header></Header>
-  <SideBar></SideBar>
-  <!-- main, router-view 들어갈 자리 -->
-  <main class="main">
-    <section class="main-content">
-      <Schedule />
-    </section>
-  </main>
+<template>
+  <div>
 
+  </div>
 </template>
 
 <script setup>
-import Header from './components/common/Header.vue';
-import SideBar from './components/common/SideBar.vue';
-import Schedule from './components/schedule/Schedule.vue';
+
 </script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "@/assets/style/mainPage.css";
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.mount("#app");
