05c2cfc9c2da33024ad73396633aa407f05675cf
minseokKim6823 <alstjr6823@naver.com>
2024-09-25 18:59:09 +0900

    스터디 참여 신청 조회 버튼 #18

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template class="root-container">
   <Navigation />
   <SideBar />
-  <main class="main">
-    <!-- router view들어갈 자리(class="main-content") -->
+  <List /><!-- <main class="main">
+    router view들어갈 자리(class="main-content") 
     <div class="main-content">
       <StudyCalendar />
     </div>
-  </main>
+  </main> -->
+
 </template>
 
 <script setup>
 import Navigation from './components/Navigation.vue';
 import SideBar from './components/SideBar.vue';
-import StudyCalendar from './views/Schedule/components/StudyCalendar.vue'; 
+import List from './components/List.vue';
+// import StudyCalendar from './views/Schedule/components/StudyCalendar.vue'; 
 </script>
 
 <style scoped></style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,35 +1,28 @@
 <template>
-	<div class="list">
-		스터디 참가 신청자 관리
-        <button class="cancel-button">취소</button>
-	</div>
+    <div class="list">
+        <div class="header-container">
+        <ListHeader />
+        <CancelButton />
+        </div>
+    </div>
 </template>
 
 <script setup>
+  import ListHeader from '@/views/Recruitment/ListHeader.vue'; // ListHeader 컴포넌트 임포트
+  import CancelButton from '@/views/Recruitment/CancelButton.vue'; // CancelButton 컴포넌트 임포트
 </script>
 
 <style scoped>
 .list {
     position: absolute;
-    top: 184px; /* 화면 위에서 184px 떨어진 위치 */
-    left: 620px; /* 가로 중앙 정렬 */
-    font-size: 40px;
-    font-family: Noto Sans;
-    font-weight: 600;
-    line-height: 60px;
-    word-wrap: break-word;
+    top: 184px; 
+    left: 620px; 
+    right: 200px; 
 }
-.cancel-button {
-    background-color: #E99A24; /* 취소 버튼 색상 */
-    color: white;
-    font-size: 24px;
-    font-family: Noto Sans;
-    font-weight: 600;
-    word-wrap: break-word;
-    width: 80px;
-    height: 48px;
-    cursor: pointer;
-    margin-top: 8px;
-    margin-left: 496px;
+.header-container {
+  display: flex; /* 플렉스 박스를 사용해 가로 배치 */
+  justify-content: space-between; /* 양 끝에 배치 */
+  align-items: center; /* 세로 중앙 정렬 */
 }
+
 </style>
