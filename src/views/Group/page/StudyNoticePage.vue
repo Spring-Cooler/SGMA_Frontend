f02bb266b71f7541e160a99abaf0a8bb77ab3262
Kibeom Jeon <kibeom1145@gmail.com>
2024-09-26 22:59:36 +0900

    Feat: 게시글 info 추가

--- src/views/Group/page/StudyNoticePage.vue
+++ src/views/Group/page/StudyNoticePage.vue
@@ -10,6 +10,11 @@
       <div class="main-content">
         <Title>스터디 공지사항</Title>
         <SearchBar></SearchBar>
+        <div class="notice-info">
+          <span>번호</span>
+          <span>제목</span>
+          <span>등록일</span>
+        </div>
       </div>
     </main>
   </div>
@@ -21,3 +26,15 @@ import GroupSideBar from '@/components/layouts/GroupSideBar.vue';
 import SearchBar from '../components/SearchBar.vue';
 import Title from '@/components/common/Title.vue';
 </script>
+
+<style>
+  .notice-info {
+    display: flex;
+    height: 9.2rem;
+    width: 96.8rem;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 2rem;
+    font-weight: 500;
+  }
+</style>
