7e38981d158eab6f20e2b501e1332874ba16334c
Kibeom Jeon <kibeom1145@gmail.com>
2024-09-26 22:30:03 +0900

    Feat: 스터디 그룹원 컴포넌트 생성

---
+++ src/components/common/MediumButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="btn small-btn">
+      {{ label }}
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: "SmallButton",
+    props: {
+      label: {
+        type: String,
+        required: true,
+      },
+    }
+  };
+  </script>
+  
+  <style scoped>
+    .small-btn {
+      height: 4.8rem;
+      width: 16rem;
+    }
+  </style>
+  
