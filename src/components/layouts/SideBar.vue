1d13fb9b9415ca2047b225689e5b78c799107922
1etterh <1etterh.dev@gmail.com>
2024-09-26 11:53:03 +0900

    Add: SchedulePage 라우팅기능

--- src/components/layouts/SideBar.vue
+++ src/components/layouts/SideBar.vue
@@ -1,20 +1,22 @@
 <template>
 	<aside class="side-nav">
-	  <div class="menu-container">
-		<SideBarMenu to="/">홈</SideBarMenu>
-		<SideBarMenu to="/study-groups">스터디 그룹 찾기</SideBarMenu>
-		<SideBarMenu to="/create-group">스터디 그룹 만들기</SideBarMenu>
-		<SideBarMenu to="/mypage">마이페이지</SideBarMenu>
-	  </div>
+		<div class="menu-container">
+			<SideBarMenu to="/">홈</SideBarMenu>
+			<SideBarMenu to="/study-groups">스터디 그룹 찾기</SideBarMenu>
+			<SideBarMenu to="/create-group">스터디 그룹 만들기</SideBarMenu>
+			<SideBarMenu to="/mypage">마이페이지</SideBarMenu>
+			<!-- 라우터 확인용 테스트 버트임(지울 예정) -->
+			<SideBarMenu to="/study-schedule">스터디 스케쥴</SideBarMenu>
+		</div>
 	</aside>
-  </template>
-  
-  <script setup>
-  import SideBarMenu from './SideBarMenu.vue';
-  </script>
-  
-  <style scoped>
-  .side-nav {
+</template>
+
+<script setup>
+import SideBarMenu from './SideBarMenu.vue';
+</script>
+
+<style scoped>
+.side-nav {
 	position: fixed;
 	left: 0px;
 	bottom: 0px;
@@ -24,13 +26,12 @@
 	height: calc(100% - 9rem);
 	background-color: #ffffff;
 	border-right: 1px solid #8c8c8c;
-  }
-  
-  .menu-container {
+}
+
+.menu-container {
 	display: flex;
 	flex-direction: column;
 	width: 100%;
 	margin-top: 5rem;
-  }
-  </style>
-  
+}
+</style>
