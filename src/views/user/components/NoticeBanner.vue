ce9588802fef097e683ee9188f923a6aebb0aa74
1etterh <1etterh.dev@gmail.com>
2024-09-26 09:48:26 +0900

    Add: notice banner에 position 속성 추가

--- src/views/user/components/NoticeBanner.vue
+++ src/views/user/components/NoticeBanner.vue
@@ -30,30 +30,42 @@
 }
 
 .notice-banner {
+  position: relative;
+  top: 9rem;
   display: flex;
-  justify-content: space-between; /* 요소들을 양쪽 끝에 정렬 */
-  align-items: center; /* 요소들을 수직 중앙 정렬 */
+  justify-content: space-between;
+  /* 요소들을 양쪽 끝에 정렬 */
+  align-items: center;
+  /* 요소들을 수직 중앙 정렬 */
   width: 100%;
-  height: 360px; /* 배너의 전체 높이 설정 */
+  height: 360px;
+  /* 배너의 전체 높이 설정 */
   background-color: #FFE083;
-  padding: 0 6rem; /* 좌우 패딩 설정 */
+  padding: 0 6rem;
+  /* 좌우 패딩 설정 */
   box-sizing: border-box;
-  font-family: 'Noto Sans', sans-serif; /* 전체 배너에 Noto Sans 적용 */
+  font-family: 'Noto Sans', sans-serif;
+  /* 전체 배너에 Noto Sans 적용 */
 }
 
 /* 왼쪽 컨텐츠 스타일 */
 .left-content {
   display: flex;
-  flex-direction: column; /* 요소들을 세로로 정렬 */
-  align-items: flex-start; /* 왼쪽 정렬 */
-  gap: 3rem; /* 요소들 사이 간격 설정 */
-  max-width: 300px; /* 최대 너비 설정 */
+  flex-direction: column;
+  /* 요소들을 세로로 정렬 */
+  align-items: flex-start;
+  /* 왼쪽 정렬 */
+  gap: 3rem;
+  /* 요소들 사이 간격 설정 */
+  max-width: 300px;
+  /* 최대 너비 설정 */
 }
 
 
 /* 안내사항 아이콘 */
 .notice-icon img {
-  width: 80px; /* 아이콘 크기 조정 */
+  width: 80px;
+  /* 아이콘 크기 조정 */
   height: auto;
   margin-top: 70px;
   margin-bottom: 30px;
@@ -61,16 +73,25 @@
 
 /* 공지사항 텍스트 스타일 */
 .notice-text {
-  font-family: 'Noto Sans', sans-serif; /* Noto Sans 폰트 적용 */
-  font-size: 28px; /* 텍스트 크기 조정 */
-  font-weight: bold; /* 굵기 조정 */
-  color: #202020; /* 텍스트 색상 설정 */
-  margin: 0; /* h2의 기본 margin 제거 */
-  display: inline; /* 인라인 요소로 설정 */
+  font-family: 'Noto Sans', sans-serif;
+  /* Noto Sans 폰트 적용 */
+  font-size: 28px;
+  /* 텍스트 크기 조정 */
+  font-weight: bold;
+  /* 굵기 조정 */
+  color: #202020;
+  /* 텍스트 색상 설정 */
+  margin: 0;
+  /* h2의 기본 margin 제거 */
+  display: inline;
+  /* 인라인 요소로 설정 */
 }
+
 .notice-text::before {
-  content: "효율적인 팀 스터디 관리, \A"; /* 줄바꿈을 의미하는 \A 추가 */
-  white-space: pre; /* 줄바꿈(\A) 적용을 위해 사용 */
+  content: "효율적인 팀 스터디 관리, \A";
+  /* 줄바꿈을 의미하는 \A 추가 */
+  white-space: pre;
+  /* 줄바꿈(\A) 적용을 위해 사용 */
 }
 
 .notice-text::after {
@@ -79,7 +100,8 @@
 
 /* 공지사항 변경 버튼 컨테이너 */
 .button-container img {
-  width: 60px; /* 버튼 이미지 크기 조정 */
+  width: 60px;
+  /* 버튼 이미지 크기 조정 */
   height: auto;
 }
 
@@ -92,7 +114,8 @@
 }
 
 .banner-image {
-  width: 220px; /* 그래프 이미지 크기 조정 */
+  width: 220px;
+  /* 그래프 이미지 크기 조정 */
   height: auto;
 }
 </style>
