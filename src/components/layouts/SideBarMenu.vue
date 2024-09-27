959f075a6596979179fbabd5d9130fb7a00e74db
woodart8 <kibeom1145@gmail.com>
2024-09-27 17:44:21 +0900

    Feat: 스터디 그룹원 추방 기능 구현

--- src/components/layouts/SideBarMenu.vue
+++ src/components/layouts/SideBarMenu.vue
@@ -20,7 +20,7 @@
   
   const router = useRouter();
   const route = useRoute();
-  const isActive = computed(() => route.path.includes(props.to));
+  const isActive = computed(() => route.path===props.to);
   
   function navigate() {
 	if (!props.to) {
