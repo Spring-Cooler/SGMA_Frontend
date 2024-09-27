356e1431abd9ff1027594aaa97884154d4223c57
1etterh <1etterh.dev@gmail.com>
2024-09-27 21:15:11 +0900

    Add: ExamPageHeader

--- src/views/Problem/ExamPage.vue
+++ src/views/Problem/ExamPage.vue
@@ -1,11 +1,27 @@
 <template>
-	<div>
-		<h1>시험 페이지</h1>
+	<div id="exam-header" class="olive">
+		<h1>올림픽 문제 스터디</h1>
 	</div>
 </template>
 
 <script setup>
-
 </script>
 
-<style scoped></style>
+<style scoped>
+#exam-header {
+	position: fixed;
+	top: 0;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	height: 20rem;
+	z-index: 1000;
+}
+
+#exam-header>h1 {
+	margin-left: 8rem;
+	font-size: 8rem;
+	color: white;
+}
+</style>
