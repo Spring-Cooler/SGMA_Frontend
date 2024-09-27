05aca5658b9be5f01ba57a7da519d785900a4b7b
1etterh <1etterh.dev@gmail.com>
2024-09-27 21:23:13 +0900

    Add: ExamTitle

--- src/views/Problem/ExamPage.vue
+++ src/views/Problem/ExamPage.vue
@@ -2,9 +2,22 @@
 	<div id="exam-header" class="olive">
 		<h1>올림픽 문제 스터디</h1>
 	</div>
+	<div class="main-content">
+		<Title>{{ problemInfos[problemIndex].problem_content }}</Title>
+	</div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import Title from '@/components/common/Title.vue';
+let problemIndex = ref(0);
+let problemInfos = ref([{
+	problemId: 1,
+	problem_content: "다음중 오륜기에 들어가는 색이 아닌 것은?",
+	problem_type: "MULTIPLE",
+	choices: ["빨간색", "파란색", "노란색", "보라색"]
+
+}])
 </script>
 
 <style scoped>
@@ -15,7 +28,7 @@
 	justify-content: space-between;
 	align-items: center;
 	width: 100%;
-	height: 20rem;
+	height: 10rem;
 	z-index: 1000;
 }
 
