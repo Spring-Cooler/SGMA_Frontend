f608542d0b47c59d268e0b0547e2a1b06f3dfd4c
minseokKim6823 <alstjr6823@naver.com>
2024-09-27 15:40:44 +0900

    Feat: 모집글 대댓글 작성 UI #35

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template class="root-container">
   <Navigation />
   <SideBar />
-  <SingleBoard />
+  <RouterView/>
 
 </template>
 
 <script setup>
 import Navigation from '@/components/layouts/Navigation.vue';
 import SideBar from '@/components/layouts/SideBar.vue';
-import SingleBoard from './views/Recruitment/SingleBoard.vue';
 
 
 </script>
